f59a5206d6ccc227c1ffe473150511268d0a566a
Renan Donizeti Carvalho dos Santos <71738231+renandonizeti@users.noreply.github.com>
2023-02-07 10:09:09 -0300

    paginas de cadastro

--- testProject/lang/en/auth.php
+++ testProject/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'E-mail ou senha incorretos, verifique seus dados.',
+    'password' => 'Senha incorreta, verifique sua senha.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
--- testProject/resources/views/dashboard.blade.php
+++ testProject/resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+           Bem vindo {{Auth::user()->name}}!
         </h2>
     </x-slot>
 
@@ -15,3 +15,5 @@
         </div>
     </div>
 </x-app-layout>
+
+
